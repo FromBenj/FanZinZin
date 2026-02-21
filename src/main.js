772f82cfe2c06a2b462c72bf5bbea416687d7cce
Benjamin TOUBOUL <touboul.benjamin@gmail.com>
2026-02-21 18:20:45 +0100

    issues with Vite & Bootstrap CSS

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
-import {getPDF} from "./js/front/custom-pdf-available.js";
-// Bootstrap CSS
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap';
 import {pdfReady} from "./js/front/animations.js";
 import {loadRightView} from "./js/front/device-check.js";
 
