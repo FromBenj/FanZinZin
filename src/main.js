84ea65ad4300476bb3c8fe85d68effafb15f32ee
Benjamin TOUBOUL <touboul.benjamin@gmail.com>
2026-02-24 17:06:16 +0100

    Updates

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import {pdfReady} from "./js/front/animations.js";
-import {loadRightView} from "./js/front/device-check.js";
-
-document.addEventListener('DOMContentLoaded', async () => {
-    await loadRightView();
-    pdfReady();
-})
